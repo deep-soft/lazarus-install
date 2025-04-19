0dc37aec361cc4869095407472c46a4fe08928cc
Alexander Koblov <alexx2000@mail.ru>
2025-04-19 13:12:18 +0300

    UPD: Install only Free Pascal

--- src/installer.ts
+++ src/installer.ts
@@ -1,13 +1,13 @@
 import * as core from "@actions/core";
 import * as lazarus from "./lazarus";
-import * as pkgs from "./packages";
+// import * as pkgs from "./packages";
 
-const REPO_BASE_URL = "https://packages.lazarus-ide.org";
-const PACKAGE_LIST_JSON = "packagelist.json";
+// const REPO_BASE_URL = "https://packages.lazarus-ide.org";
+// const PACKAGE_LIST_JSON = "packagelist.json";
 
 export class Installer {
   private lazarus: lazarus.Lazarus;
-  private packages: pkgs.Packages;
+  // private packages: pkgs.Packages;
   private packageList: string[];
 
   constructor(
@@ -17,11 +17,13 @@ export class Installer {
   ) {
     this.lazarus = new lazarus.Lazarus(lazarusVersion, useCache);
     this.packageList = packageList;
+    /*
     this.packages = new pkgs.Packages(
       lazarusVersion,
       REPO_BASE_URL,
       PACKAGE_LIST_JSON
     );
+    */
   }
 
   async install(): Promise<void> {
@@ -37,7 +39,7 @@ export class Installer {
       if (this.packageList.length > 0) {
         core.startGroup("Installing Additional Packages");
         core.info(`Packages to install: ${this.packageList.join(", ")}`);
-        await this.packages.installPackages(this.packageList);
+        // await this.packages.installPackages(this.packageList);
         core.endGroup();
         core.info("Package installation completed.");
       } else {
