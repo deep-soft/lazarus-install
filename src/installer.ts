21ba9306173a1a8b0f25f402595ddf38dcb121e9
Alexander Koblov <alexx2000@mail.ru>
2025-04-20 16:05:06 +0300

    FIX: Don't load a package list if not required

--- src/installer.ts
+++ src/installer.ts
@@ -1,13 +1,13 @@
 import * as core from "@actions/core";
 import * as lazarus from "./lazarus";
-// import * as pkgs from "./packages";
+import * as pkgs from "./packages";
 
-// const REPO_BASE_URL = "https://packages.lazarus-ide.org";
-// const PACKAGE_LIST_JSON = "packagelist.json";
+const REPO_BASE_URL = "https://packages.lazarus-ide.org";
+const PACKAGE_LIST_JSON = "packagelist.json";
 
 export class Installer {
   private lazarus: lazarus.Lazarus;
-  // private packages: pkgs.Packages;
+  private packages: pkgs.Packages;
   private packageList: string[];
 
   constructor(
@@ -17,13 +17,11 @@ export class Installer {
   ) {
     this.lazarus = new lazarus.Lazarus(lazarusVersion, useCache);
     this.packageList = packageList;
-    /*
     this.packages = new pkgs.Packages(
       lazarusVersion,
       REPO_BASE_URL,
       PACKAGE_LIST_JSON
     );
-    */
   }
 
   async install(): Promise<void> {
@@ -36,10 +34,10 @@ export class Installer {
       core.endGroup();
       core.info("Lazarus installation completed.");
 
-      if (this.packageList.length > 0) {
+      if ((this.packageList.length > 0) && (this.packageList[0].length > 0)) {
         core.startGroup("Installing Additional Packages");
         core.info(`Packages to install: ${this.packageList.join(", ")}`);
-        // await this.packages.installPackages(this.packageList);
+        await this.packages.installPackages(this.packageList);
         core.endGroup();
         core.info("Package installation completed.");
       } else {
